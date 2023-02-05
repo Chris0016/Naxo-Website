cd4daa4eb4e91f6fed9432da9d339bbf73b6b18e
w0terMonk <notARandomEmail@gmail.com>
2023-02-05 02:58:12 -0500

    Initial Commit

---
+++ background.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".bckg-img-1").hover(function(){
+        $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg.jpeg') no-repeat center center fixed ");
+    });
+    
+    $(".bckg-img-2").hover(function(){
+        $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg2.jpg') no-repeat center center fixed ");
+    });
+
+    $(".bckg-img-3").hover(function(){
+        $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg3.jpeg') no-repeat center center fixed ");
+    });
+
+    $(".bckg-img-4").hover(function(){
+        $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg4.jpg') no-repeat center center fixed ");
+    });
+});
