003fb7157938670c9e2905ee04436f8a2ab10f37
w0terMonk <notARandomEmail@gmail.com>
2023-02-09 02:57:50 -0500

    Bio Mult-Media Presskit

--- background.js
+++ background.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
     $(".bckg-img-1").hover(function(){
-        $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg.jpeg') no-repeat center center fixed ");
+        $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg.jpeg') no-repeat center center fixed");
+        $('.body-bckg').css("background-size", "cover");
+        $('.body-bckg').css("background-position-y", "-350px");
+        $('.body-bckg').css("background-position-x", "center");
     });
     
     $(".bckg-img-2").hover(function(){
         $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg2.jpg') no-repeat center center fixed ");
+        // $('.body-bckg').css("background-size", "cover");
     });
 
     $(".bckg-img-3").hover(function(){
         $('.body-bckg').css("background", "linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),  url('./main-bckg3.jpeg') no-repeat center center fixed ");
+        $('.body-bckg').css("background-size", "cover");
     });
 
     $(".bckg-img-4").hover(function(){
